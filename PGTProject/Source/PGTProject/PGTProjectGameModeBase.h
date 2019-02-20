371e7358f8cef45e1a4f2d049b809eb3cdd93699
Stefan Djordjevic <stefan_djordjevic1991@hotmail.com>
2019-02-20 12:51:58 +0100

    Empty Project

---
+++ PGTProject/Source/PGTProject/PGTProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PGTProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PGTPROJECT_API APGTProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

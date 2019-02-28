f8e9fd6fb5cc6622fca61807ade61e2a153db776
Stefan Djordjevic <stefan_djordjevic1991@hotmail.com>
2019-02-28 13:27:46 +0100

    Added a Camera w/ panning

---
+++ PGTProject/Source/PGTProject/CameraPawn.cpp
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CameraPawn.h"
+
+// Sets default values
+ACameraPawn::ACameraPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Sets camera controls ro player0 otherwise it crashes the editor
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	RootComponent = RootScene;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootScene);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->SetRelativeRotation(FRotator(-50, 0, 0));
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComp->SetupAttachment(SpringArm);
+}
+
+// Called when the game starts or when spawned
+void ACameraPawn::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PC = Cast<APlayerController>(GetController());
+	PC->GetViewportSize(screenSizeX, screenSizeY);
+	
+}
+
+// Called every frame
+void ACameraPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AddActorWorldOffset(GetCameraPanDirection() * camSpeed);
+
+}
+
+// Called to bind functionality to input
+void ACameraPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+FVector ACameraPawn::GetCameraPanDirection()
+{
+	float mousePosX;
+	float mousePosY;
+	float camDirectionX = 0;
+	float camDirectionY = 0;
+
+	PC->GetMousePosition(mousePosX, mousePosY);
+
+	// Pan left
+	if (mousePosX <= margin)
+	{
+		camDirectionY = -1;
+	}
+
+	// Pan up
+	if (mousePosY <= margin)
+	{
+		camDirectionX = 1;
+	}
+
+	// Pan right
+	if (mousePosX >= screenSizeX - margin)
+	{
+		camDirectionY = 1;
+	}
+
+	// Pan down
+	if (mousePosY >= screenSizeY - margin)
+	{
+		camDirectionX = -1;
+	}
+
+	return FVector(camDirectionX, camDirectionY, 0);
+}
+
---
+++ PGTProject/Source/PGTProject/CameraPawn.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "CameraPawn.generated.h"
+
+
+UCLASS()
+class PGTPROJECT_API ACameraPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACameraPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY()
+		USceneComponent* RootScene;
+
+
+		USpringArmComponent* SpringArm;
+
+
+		UCameraComponent* CameraComp;
+
+
+		APlayerController* PC;
+
+		float margin = 15;
+
+
+		int32 screenSizeX;
+
+
+		int32 screenSizeY;
+
+
+		float camSpeed = 3;
+	
+
+	UFUNCTION()
+	FVector GetCameraPanDirection();
+
+	
+	
+};

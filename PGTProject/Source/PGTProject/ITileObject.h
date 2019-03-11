d43311f9c4faaf9db555259a726db575a1c9f095
SoaringDylan <dylan@fampiera.nl>
2019-03-11 13:39:13 +0100

    Add ITileObject

---
+++ PGTProject/Source/PGTProject/ITileObject.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "HexActor.h"
+
+class ITileObject
+{
+public:
+	ITileObject(const AHexActor::Hex& hex, const UINT32 starCost)
+		: _hex(hex),
+		  _aStarCost(starCost) {}
+
+	/**
+	 * \brief 
+	 * A reference to the hex tile equivalent
+	 */
+	AHexActor::Hex _hex;
+
+	//TODO: add reference to HexManager
+
+	/**
+	 * \brief 
+	 * The cost to enter a tile based on the A* algorithm
+	 */
+	const UINT32 _aStarCost;
+
+	
+
+};

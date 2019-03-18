06395ac6165706820ea24b87db1cd98adec405ae
Martijn van der Meer <martijn.vander.meer@quicknet.nl>
2019-03-18 10:35:59 +0100

    Visible Mouse
    Added mouse that is visible in-game

--- PGTProject/Source/PGTProject/PGTProjectGameModeBase.cpp
+++ PGTProject/Source/PGTProject/PGTProjectGameModeBase.cpp
@@ -1,7 +1,10 @@
 #include "PGTProjectGameModeBase.h"
-#include  "GameHUD.h"
+#include "GameHUD.h"
+#include "MouseController.h"
 
 APGTProjectGameModeBase::APGTProjectGameModeBase() : Super()
 {
+	//Setting HUD and Mouse to be standard HUD/Mouse
 	HUDClass = AGameHUD::StaticClass();
+	PlayerControllerClass = AMouseController::StaticClass();
 }

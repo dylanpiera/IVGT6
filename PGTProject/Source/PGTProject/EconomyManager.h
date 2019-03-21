65e29d6273cc1faddb80b52e69577dbee0d0585b
SoaringDylan <dylan@fampiera.nl>
2019-03-21 10:13:09 +0100

    Add EconomyManager.h

---
+++ PGTProject/Source/PGTProject/EconomyManager.h
@@ -0,0 +1,22 @@
+#pragma once
+
+//TODO: Discuss if this should be a uentity
+
+namespace economy_manager
+{
+	class EconomyManager {
+	public:
+		/**
+		 * \brief 
+		 * talk about shit here
+		 */
+		struct Resources
+		{
+			int _energy;
+			int _materials;
+			int _money;
+
+			explicit Resources(const int energy = 0, const int materials = 0, const int money = 0) : _energy(energy), _materials(materials), _money(money) { }
+		};
+	};
+};

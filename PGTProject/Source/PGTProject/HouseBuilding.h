e3edb25206dd823902aec692455269cc894b5784
Martijn van der Meer <martijn.vander.meer@quicknet.nl>
2019-05-27 15:23:32 +0200

    Add Disdain

--- PGTProject/Source/PGTProject/HouseBuilding.h
+++ PGTProject/Source/PGTProject/HouseBuilding.h
@@ -4,8 +4,17 @@
 class HouseBuilding : public Building {
 	int32 PopulationHousingValue = 10;
 	int EnergyUpkeep = 10;
+	int Disdain = 0;
 
 	void BuildingFunction(Resources& resource) override {
 		resource._energy -= EnergyUpkeep;
+		if (resource._food == 0)
+		{
+			Disdain++;
+		}
+		else if (Disdain != 0)
+		{
+			Disdain--;
+		}
 	}
 };
